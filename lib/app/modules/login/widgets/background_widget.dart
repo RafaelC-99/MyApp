7c661ef9db0226c5a8140e253a882ce169d0872d
Rafael A <rafaelcardosocatuti@gmail.com>
2023-01-03 02:32:10 -0300

    Excluindo parte comentada

--- lib/app/modules/login/widgets/background_widget.dart
+++ lib/app/modules/login/widgets/background_widget.dart
@@ -17,21 +17,3 @@ class BackgroundLogin extends StatelessWidget {
     );
   }
 }
-
-// class Background extends StatelessWidget {
-//   const Background({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         image: DecorationImage(
-//           fit: BoxFit.cover,
-//           image: AssetImage(
-//             'assets/pictures/login_web_bg.jpg',
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
